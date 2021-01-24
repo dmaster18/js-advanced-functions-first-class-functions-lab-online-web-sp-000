ecbfae0dffa66c6937ca28641ac9f198b6a059d8
David Strauch <dmaster18@gmail.com>
2021-01-24 04:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 // Code your solution in this file!
-
-const returnFirstTwoDrivers = function(arrayOfDrivers) {
-  return [arrayOfDrivers[0], arrayOfDrivers[1]];
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2);
 }
 
-const returnLastTwoDrivers = function(arrayOfDrivers) {
-  return [arrayOfDrivers[arrayOfDrivers.length-2], arrayOfDrivers[arrayOfDrivers.length - 1]];
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
 }
 
-function selectingDrivers(arrayOfDrivers) {
-  return [returnFirstTwoDrivers(arrayOfDrivers), returnLastTwoDrivers(arrayOfDrivers)];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return function(fair){
+        return fair*multiplier
+    }
 }
 
-function createFareMultiplier(multiplier) {
-  return function(fare) {multiplier*fare;}
-}
+const fareDoubler = createFareMultiplier(2)
 
-const fareDoubler = createFareMultiplier(2)();
+const fareTripler = createFareMultiplier(3)
 
-const fareTripler = createFareMultiplier(3)();
-
-function selectDifferentDrivers(arrayOfDrivers, firstTwoOrLast) {
-  if (firstTwoOrLast === returnFirstTwoDrivers(arrayOfDrivers)) {
-    return returnFirstTwoDrivers;
-  } else {
-    return returnLastTwoDrivers(arrayOfDrivers);
-  }
+const selectDifferentDrivers = function(array, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(array)
 }
